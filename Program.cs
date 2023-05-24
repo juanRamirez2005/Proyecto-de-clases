de8fcab612427626b217617526d05c5b8d6c9309
juan ramirez arias <jcra.juancamilo@gmail.com>
2023-05-24 15:52:46 -0500

    añadiendo un condicional para identificar quien es mayor de edad o menor de edad

--- Program.cs
+++ Program.cs
@@ -10,14 +10,18 @@ namespace Constructores
             {
                 new Person("Juan Camilo", 18),
                 new Person("Hector Andres", 34),
-                new Person("Sandra Milena", 40)
+                new Person("Sandra Milena", 40),
+                new Person("Luisa Maria", 14),
+                new Person("Daniel", 14),
+                new Person("Santiago", 19),
+                new Person("Pablo", 12),
             };
 
             People myclass = new People(initialPeople);
 
-            foreach(Person person in initialPeople) 
+            foreach (Person person in initialPeople)
             {
-                Console.WriteLine($"Name: {person.Name} Age: {person.Age}");
+                Console.WriteLine("Name: " + person.Name + " |Age: " + person.Age + (person.Age > 18? " --This person is an Adult": " --This person is Underage"));
             }
         }
     }
